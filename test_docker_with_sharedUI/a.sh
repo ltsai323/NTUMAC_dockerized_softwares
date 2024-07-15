9f6e5431585e27200c08bd1ccc352042f554d873
ltsai323 <johnson20050@gmail.com>
2024-07-15 21:48:24 +0800

    sync

--- test_docker_with_sharedUI/a.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-if [ -z "$SCRIPT_PATH" ]; then
-  echo "SCRIPT_PATH environment variable is required"
-  exit 1
-fi
-python3 $SCRIPT_PATH
-echo PyQT5 application closed. Keeping container running...
-tail -f /dev/null
-
