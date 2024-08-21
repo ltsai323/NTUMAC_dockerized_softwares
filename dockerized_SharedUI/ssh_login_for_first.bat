db874ed789d5e9bcf59a2dad17def4d6e0f649d4
ltsai323 <johnson20050@gmail.com>
2024-08-21 16:54:00 +0800

    additional script solving the SSH initialization

---
+++ dockerized_SharedUI/ssh_login_for_first.bat
@@ -0,0 +1,9 @@
+@echo off
+REM Prompt the user to enter their username
+echo Please login LXPlus first
+set /p username=Please enter lxplus username:
+
+ssh %username%@%server%
+scp -o ProxyJump=%username%@lxplus.cern.ch %username%@dbloader-hgcal 'echo -e "\n\n    Login Successfully \n\n"'
+
+pause
