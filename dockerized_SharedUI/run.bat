f5e0d0246bfc7a49ac416223e4cf63ba0ccf2f60
ltsai323 <johnson20050@gmail.com>
2024-08-21 16:54:31 +0800

    include .ssh directory to docker container for known_hosts

--- dockerized_SharedUI/run.bat
+++ dockerized_SharedUI/run.bat
@@ -3,6 +3,7 @@ docker run -it --rm ^
   --env="QT_X11_NO_MITSHM=1" ^
   --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" ^
   --volume="%cd%\filemanager_data:/SharedUI/filemanager_data" ^
+  --volume "%USERPROFILE%\.ssh:/root/.ssh:rw" ^
   --name shared-ui-container shared-ui-image
 
 echo FINISHED
