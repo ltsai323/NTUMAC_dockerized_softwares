db7485bf4b9704f4aa1c026d58311f9633bb0a51
ltsai323 <johnson20050@gmail.com>
2024-09-13 11:07:17 +0800

    check docker desktop running or not

--- dockerized_SharedUI/init.bat
+++ dockerized_SharedUI/init.bat
@@ -1,3 +1,24 @@
+@echo off
+
+:: Check if Docker is running
+docker info >nul 2>&1
+if %ERRORLEVEL% neq 0 (
+    echo Docker is not running. Starting Docker Desktop...
+    start "" "C:\Program Files\Docker\Docker\Docker Desktop.exe"
+    echo Waiting for Docker Desktop standby...
+    timeout /t 30
+
+    :: Check again
+    docker info >nul 2>&1
+    if %ERRORLEVEL% neq 0 (
+        echo [ERROR]
+        echo [ERROR] Unable to activate Docker Desktop... Please run Docker Desktop manually.
+        echo [ERROR]
+        pause
+        exit /b 1
+    )
+)
+
 docker rmi shared-ui-image
 docker build -t shared-ui-image .
 
