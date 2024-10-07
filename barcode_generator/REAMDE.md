cc812162e1bdf4e44021f9b1167cee431f46c78a
ltsai323 <johnson20050@gmail.com>
2024-10-07 15:19:28 +0800

    add kind_of_parts checking and make layout horizontal layout

--- barcode_generator/REAMDE.md
+++ barcode_generator/REAMDE.md
@@ -1,2 +1,8 @@
 # Barcode Generator
 A Tkinter GUI generating Barcode for HGCal Database
+
+
+
+
+### kind_of_parts.csv
+Downloded from (Ali's Repository)[https://gitlab.cern.ch/hgcal-database/usefull-scripts/-/blob/master/data/kind_of_parts.csv]
--- barcode_generator/mainfunc.py
+++ barcode_generator/mainfunc.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 from tkinter import ttk
 import yaml
+from pprint import pprint
+
 
 yaml_pool = {
         'Sensor':'data/subtype_sensor.yaml',
@@ -9,6 +11,7 @@ yaml_pool = {
         'Baseplate': 'data/subtype_baseplate.yaml',
         'Silicon Module': 'data/subtype_simodule.yaml',
         }
+kind_of_parts_pool = [ 'data/kind_of_parts.csv', 'data/kind_of_parts_implement.csv' ]
 # Sample YAML data as a string (this would typically be loaded from a file)
 yaml_data = '''
 code_structure:
@@ -67,7 +70,8 @@ Digits:
 # Load YAML content based on the selected file
 
 # Function to update the GUI based on YAML content
-def update_options(yamlFILE):
+def yamlSelection_update_options(yamlFILE):
+    global gColIdx
     # Clear existing options
     for widget in subtype_frame.winfo_children():
         widget.destroy()
@@ -76,19 +80,19 @@ def update_options(yamlFILE):
     selected_values.clear()  # List to store all selected values (StringVars). The content should be StringVars or a list of StringVars
     digits_before_id_value.clear()
     post_func_checking.clear()
-    irow = 0 # start from 1. row 0 is the result row
+    gColIdx = 0 # start from 1. row 0 is the result row
     main_option_menu(yamlFILE)
 
 # Event handler when a YAML file is selected
-def on_yaml_selection():
+def yamlSelection_on_yaml_selection():
     selected_file = selected_yaml.get()
-    update_options(selected_file)
+    yamlSelection_update_options(selected_file)
 
 
-def update_textbox():
+def update_ID():
     """Update the textbox with selected values from the radio buttons."""
-    global text_box
-    text_box.delete(1.0, tk.END)  # Clear the textbox
+    global variableText_generatedID
+    variableText_generatedID.delete(1.0, tk.END)  # Clear the textbox
     selected_text = ""
     sel_vals = []
     for sel_val in selected_values:
@@ -112,74 +116,114 @@ def update_textbox():
     overall_digits = digits_before_id + f'{intval:0{number_of_digits}d}'
     selected_text += f'-{overall_digits}'  # Add integer value
     #print(f'[selected_text] {selected_text}')
-    text_box.insert(tk.END, selected_text)  # Insert the text into the textbox
+    variableText_generatedID.insert(tk.END, selected_text)  # Insert the text into the textbox
+
+    # generate KIND_OF_PARTS
+    global strVar_KIND_OF_PARTS, kindOfPartsLibrary
+    generatedKindOfParts = get_kind_of_parts_from_ID(kindOfPartsLibrary, selected_text)
+
+    strVar_KIND_OF_PARTS.set(generatedKindOfParts)
 
 
 def copy_to_clipboard():
     """Copy the contents of the textbox to the clipboard."""
     root.clipboard_clear()
-    root.clipboard_append(text_box.get(1.0, tk.END))  # Copy text to clipboard
-    print(f'[CopiedID] {text_box.get(1.0,tk.END)}')
+    root.clipboard_append(variableText_generatedID.get(1.0, tk.END))  # Copy text to clipboard
+    print(f'[CopiedID] {variableText_generatedID.get(1.0,tk.END)}')
+def copy_to_clipboard_KIND_OF_PARTS():
+    """Copy the contents of the textbox to the clipboard."""
+    root.clipboard_clear()
+    root.clipboard_append(strVar_KIND_OF_PARTS.get(1.0, tk.END))  # Copy text to clipboard
+    print(f'[CopiedID] {strVar_KIND_OF_PARTS.get(1.0,tk.END)}')
 
+
+
+######### ID Validator generates KIND_OF_PARTS #########
+def read_kind_of_parts_list(csvFILEs):
+    import csv
+    class CSVEntry:
+        def __init__(self, csvENTRY):
+            self.n = csvENTRY['DISPLAY_NAME']
+            self.t = csvENTRY['LABEL_TYPECODE']
+            self.l = csvENTRY['LPNAME']
+        def __repr__(self): return self.n
+    
+    kind_of_parts_library = []
+    for csvFILE in csvFILEs:
+        with open(csvFILE,'r') as csvFile:
+            filtered_file = ( line for line in csvFile if not line.lstrip().startswith('#') )
+            d = csv.DictReader(filtered_file)
+            for row in d:
+                kind_of_parts_library.append(CSVEntry(row))
+
+    return kind_of_parts_library
+    pprint(kind_of_parts_library)
+def get_kind_of_parts_from_ID(kindOFpartsLIBRARY:list, generatedID:str) -> str:
+    for kind_of_part_entry in kindOFpartsLIBRARY:
+        if kind_of_part_entry.t in generatedID:
+            return kind_of_part_entry.n
+    #return 'INVALID ID'
+    return 'KIND_OF_PART not found (Further implementation required)'
+            
+######### ID Validator generates KIND_OF_PARTS end #########
+#########
 
 def assembly_code(codeDICT:dict):
-    global root, irow
+    global root, gColIdx
 
     sel_vals = []
+    gColIdx += 1  # Move to the next row for the next set of options
     for subtitle, subopt in codeDICT.items():
-        # Add a horizontal separator before the label
-        separator = ttk.Separator(subtype_frame, orient='horizontal')
-        separator.grid(row=irow, column=0, columnspan=max_column, sticky="ew", pady=10)  # Spans 5 columns and adds vertical padding
-        irow += 1  # Move to the next row for the label
+        irow = 0
+        # # Add a horizontal separator before the label
+        # separator = ttk.Separator(subtype_frame, orient='horizontal')
+        # separator.grid(row=irow, column=gColIdx, columnspan=max_column, sticky="ew", pady=10)  # Spans 5 columns and adds vertical padding
 
         # Label with padding before it
         label = ttk.Label(subtype_frame, text=subtitle)
-        label.grid(row=irow, column=0, padx=5, sticky="w")  # Add padding and align label to the left
+        label.grid(row=irow, column=gColIdx, padx=5)  # Add padding and align label to the left
         label.config(font=font_subtitle)
-        irow += 1  # Move to the next row for the Radiobuttons
+        irow += 1
 
         # Radiobuttons in the next row
         selected_value = tk.StringVar()
         #selected_values.append(selected_value)  # Add the StringVar to the list
         sel_vals.append(selected_value)  # Add the StringVar to the list
-        icol = 0
+        first_item = True
         for code, illustration in subopt.items():
             #radio = tk.Radiobutton(root, text=f'{illustration}', variable=selected_value, value=f'{code}',
-            #                       command=update_textbox)
-            radio = tk.Radiobutton(subtype_frame, text=f'{illustration}', variable=selected_value, value=f'{code}', wrap=150, command=update_textbox)
-            radio.grid(row=irow, column=icol, padx=20, sticky='w')  # Add small padding between Radiobuttons
+            #                       command=update_ID)
+            radio = tk.Radiobutton(subtype_frame, text=f'{illustration}', variable=selected_value, value=f'{code}', wrap=150, command=update_ID)
+            radio.grid(row=irow, column=gColIdx, sticky='w')  # Add small padding between Radiobuttons
             radio.config(font=font_options)
-            if icol == 0: radio.select()
-            icol += 1
-        irow += 1  # Move to the next row for the next set of options
+            if first_item:
+                radio.select()
+                first_item = False
+            irow += 1
+        gColIdx += 1
     selected_values.append(sel_vals)
 
 
 def single_code(codeDICT:dict):
-    global root, irow
-    # Add a horizontal separator before the label
-    #separator = ttk.Separator(subtype_frame, orient='horizontal')
-    #separator.grid(row=irow, column=0, columnspan=max_column, sticky="ew", pady=10)  # Spans 5 columns and adds vertical padding
-    #irow += 1  # Move to the next row for the label
-
+    global root, gColIdx, gRowIdx
     # Label with padding before it
     selected_value = tk.StringVar()
     selected_values.append(selected_value)  # Add the StringVar to the list
-    #label = ttk.Label(subtype_frame, text="Options:")
-    #label.grid(row=irow, column=0, padx=5, sticky="w")  # Add padding and align label to the left
-    #label.config(font=font_subtitle)
-    irow += 1  # Move to the next row for the Radiobuttons
+    # gColIdx += 1  # Move to the next row for the Radiobuttons
 
     # Radiobuttons in the next row
-    icol = 0
+    first_item = True
     for code, illustration in codeDICT.items():
+        #gColIdx+=1
         radio = tk.Radiobutton(subtype_frame, text=f'{illustration}', variable=selected_value, value=f'{code}',
-                               command=update_textbox)
-        radio.grid(row=irow, column=icol, padx=20, sticky='w')  # Add small padding between Radiobuttons
+                               command=update_ID)
+        radio.grid(row=gRowIdx, column=gColIdx, sticky='w')  # Add small padding between Radiobuttons
         radio.config(font=font_options)
-        if icol == 0: radio.select() # select first column as the default value
-        icol += 1
-    irow += 1
+        if first_item:
+            radio.select() # select first column as the default value
+            first_item = False
+        gRowIdx += 1
+    gRowIdx += 1
 
 def ID_on_entry_click(event):
     """Clear the default text when the entry field is clicked."""
@@ -202,9 +246,10 @@ def ID_validate_input(value_if_allowed):
     except ValueError:
         return False  # Disallow non-integer values
 def ID_on_enter(event):
-    update_textbox()
+    update_ID()
 
 def main_option_menu(yamlFILE):
+    global gColIdx
 ########## Row 1~N-1 selected options from yaml file ##########
 # Load YAML data
     f = open(yamlFILE)
@@ -232,20 +277,39 @@ def main_option_menu(yamlFILE):
         
         if isinstance(first_item, dict):
             assembly_code(code_dict)
+            global gRowIdx
+            gRowIdx = 1 # set to default
         else:
             single_code(code_dict)
 ########## Row 1~N-1 selected options from yaml file ##########
 
 ########## Row N: Result message box and copy button ########## 
-    global text_box
-    text_box = tk.Text(subtype_frame, height=5, width=50)
-    text_box.grid(row=irow, column=1, columnspan=max_column-1, padx=10, pady=10, sticky="ew")
-    text_box.delete(1.0, tk.END)  # Clear the textbox
+    gColIdx = 98
+    global variableText_generatedID
+    variableText_generatedID = tk.Text(subtype_frame, height=5, width=50)
+    variableText_generatedID.grid(row=gColIdx, column=1, columnspan=max_column-1, padx=10, pady=10, sticky="ew")
+    variableText_generatedID.delete(1.0, tk.END)  # Clear the textbox
 
 # Button to copy the text in the TextBox to clipboard
     copy_button = tk.Button(subtype_frame, text="Copy to Clipboard", command=copy_to_clipboard, wrap=100)
-    copy_button.grid(row=irow, column=0, padx=10, pady=10)
+    copy_button.grid(row=gColIdx, column=0, padx=10, pady=10)
 ########## Row N: Result message box and copy button end ########## 
+
+
+########## Row N+1: Generate KIND_OF_PARTS from ID ##########
+    #gColIdx+=1
+    gColIdx = 99
+    global strVar_KIND_OF_PARTS
+    strVar_KIND_OF_PARTS = tk.StringVar()
+    strVar_KIND_OF_PARTS.set('KIND_OF_PARTS')
+    label_KIND_OF_PARTS = tk.Label(subtype_frame, textvariable=strVar_KIND_OF_PARTS)
+    label_KIND_OF_PARTS.grid(row=gColIdx, column=1, padx=10,pady=10, columnspan=max_column-1)
+
+    
+# Button to copy the text in the TextBox to clipboard
+    copy_button_KIND_OF_PARTS = tk.Button(subtype_frame, text="Copy to Clipboard", command=copy_to_clipboard_KIND_OF_PARTS, wrap=100)
+    copy_button_KIND_OF_PARTS.grid(row=gColIdx, column=0, padx=10, pady=10)
+########## Row N+1: Generate KIND_OF_PARTS from ID end ##########
 
 
 ########## Row0: Handling Digits part ##########
@@ -285,6 +349,9 @@ Digits:
 ########## Row0: Handling Digits part end ##########
 
 if __name__ == "__main__":
+    kindOfPartsLibrary = read_kind_of_parts_list(kind_of_parts_pool)
+
+
     font_subtitle = ('Arial', 20)
     font_options = ('Arial', 10)
     max_column = 20
@@ -303,7 +370,7 @@ if __name__ == "__main__":
 
     selected_yaml = tk.StringVar()
     for name, yaml_file in yaml_pool_.items():
-        ttk.Radiobutton(yaml_frame, text=name, value=yaml_file, variable=selected_yaml, command=on_yaml_selection).pack(anchor=tk.W, fill=tk.Y, side=tk.LEFT)
+        ttk.Radiobutton(yaml_frame, text=name, value=yaml_file, variable=selected_yaml, command=yamlSelection_on_yaml_selection).pack(anchor=tk.W, fill=tk.Y, side=tk.LEFT)
 # Frame for dynamic options based on YAML content
 ####### Load list_of_sources.yaml choose generator type. Baseplate / Hexaboard / Module ... etc END ####### 
 
@@ -337,7 +404,9 @@ if __name__ == "__main__":
 
     digits_before_id_value = []
     post_func_checking = []
-    irow = 0 # start from 1. row 0 is the result row
-    text_box = None
+    gColIdx = 0 # start from 1. row 0 is the result row
+    gRowIdx = 1
+    variableText_generatedID = None
+    strVar_KIND_OF_PARTS = None
     #subtype_frame.columnconfigure(8, weight=1)
     root.mainloop()
